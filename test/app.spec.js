1cdc2f3cb0a19688802927b698131685a457c247
rysiu21 <bachara27492@student.pwsztar.edu.pl>
2017-04-26 12:28:40 +0200

    first test

--- test/app.spec.js
+++ test/app.spec.js
@@ -5,15 +5,35 @@ var app = require('../app/app');
 
 describe('reverseNumber', function ()
 {
+    it('should reverse', function () {
+        expect(app.reverseNumber(691)).to.eql(196);
+    })
+    /*it('if is not number', function () {
+        expect(app.reverseNumber('2I')).to.eql(false);
+    })*/
 
 });
 
 describe('returnOnlyLetter', function ()
 {
+    it('should return only letter',function () {
+        expect(app.returnOnlyLetter('ry3siu21')).to.eql('rysiu');
+    })
+    /*it('should return false',function () {
+        expect(app.returnOnlyLetter(669)).to.eql(false);
+    })
+    it('should return only letter',function () {
+        expect(app.returnOnlyLetter('xyz')).to.eql('xyz');
+    })*/
 
 });
 
 describe('isEmail', function ()
 {
-
+    it('should return true',function () {
+        expect(app.isEmail('gdybym@mialgitare.pl')).to.eql(true);
+    })
+    /*it('should return false',function () {
+        expect(app.isEmail(gdybymmialgitare.pl)).to.eql(false);
+    })*/
 });
