4015785316fdf2a225bf4697d67110bddf57ac0c
rysiu21 <bachara27492@student.pwsztar.edu.pl>
2017-04-26 12:52:25 +0200

    second test

--- test/app.spec.js
+++ test/app.spec.js
@@ -8,9 +8,12 @@ describe('reverseNumber', function ()
     it('should reverse', function () {
         expect(app.reverseNumber(691)).to.eql(196);
     })
-    /*it('if is not number', function () {
+    it('if is not number', function () {
         expect(app.reverseNumber('2I')).to.eql(false);
-    })*/
+    })
+    it('if return hex',function () {
+        expect(app.reverseNumber(0x10)).to.eql(0x3D)
+    })
 
 });
 
@@ -19,12 +22,15 @@ describe('returnOnlyLetter', function ()
     it('should return only letter',function () {
         expect(app.returnOnlyLetter('ry3siu21')).to.eql('rysiu');
     })
-    /*it('should return false',function () {
+    it('should return false',function () {
         expect(app.returnOnlyLetter(669)).to.eql(false);
     })
     it('should return only letter',function () {
         expect(app.returnOnlyLetter('xyz')).to.eql('xyz');
-    })*/
+    })
+    it('if return hex',function () {
+        expect(app.returnOnlyLetter('')).to.eql('')
+    })
 
 });
 
@@ -33,7 +39,7 @@ describe('isEmail', function ()
     it('should return true',function () {
         expect(app.isEmail('gdybym@mialgitare.pl')).to.eql(true);
     })
-    /*it('should return false',function () {
-        expect(app.isEmail(gdybymmialgitare.pl)).to.eql(false);
-    })*/
+    it('should return false',function () {
+        expect(app.isEmail('gdybymmialgitare.pl')).to.eql(false);
+    })
 });
